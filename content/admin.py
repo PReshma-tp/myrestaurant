30665bdd96d1f05aa8b6369c3530eeb3b5bcc4a8
PReshma-tp <reshma@testpress.in>
2025-07-30 10:47:56 +0000

    add models in admin site

--- content/admin.py
+++ content/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Review, Photo
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("user", "content_type", "object_id", "rating", "created_at")
+    list_filter = ("rating", "created_at")
+    search_fields = ("user__username", "comment")
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ("uploaded_by", "content_type", "object_id", "image", "uploaded_at")
+    list_filter = ("uploaded_at",)
--- restaurants/admin.py
+++ restaurants/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.db.models import Avg
+from .models import Cuisine, Restaurant, MenuItem
 
-# Register your models here.
+@admin.register(Cuisine)
+class CuisineAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+@admin.register(Restaurant)
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ("name", "city", "cost_for_two", "avg_rating_display", "spotlight")
+    list_filter = ("spotlight", "city", "veg_type")
+    search_fields = ("name", "city")
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        
+        return qs.annotate(avg_rating= Avg("reviews__rating"))
+
+    def avg_rating_display(self, obj):
+        return round(obj.avg_rating or 0, 1)
+    avg_rating_display.short_description = "Rating"
+    avg_rating_display.admin_order_field = "avg_rating" 
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("name", "restaurant", "price", "cuisine")
+    list_filter = ("cuisine", "restaurant")
+    search_fields = ("name",)
