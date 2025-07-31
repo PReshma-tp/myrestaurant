c456625f3b6cbcf34dbdc85c54a3a9465add0c89
PReshma-tp <reshma@testpress.in>
2025-07-31 10:03:58 +0530

    Feat: register all models in django admin (#3)
    * add models in admin site
    
    * add an extra empty line in admin.py files
    
    * add empty line

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Bookmark, Visited
 
-# Register your models here.
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ("user", "restaurant", "created_at")
+    list_filter = ("created_at",)
+    search_fields = ("restaurant__name", "user__username")
+
+@admin.register(Visited)
+class VisitedAdmin(admin.ModelAdmin):
+    list_display = ("user", "restaurant", "visited_on")
+    list_filter = ("visited_on",)
+    search_fields = ("restaurant__name", "user__username")
