0a25d52e8112febbf3c511509da7d89c3aa9d586
PReshma-tp <reshma@testpress.in>
2025-08-04 12:46:50 +0000

    add redirection urls

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -117,6 +117,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Redirection Urls
+
+LOGIN_URL = 'accounts:login'
+
+LOGIN_REDIRECT_URL = 'restaurants:restaurant_list'
+
+LOGOUT_REDIRECT_URL = 'accounts:login'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
