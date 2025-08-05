4f9eafba65af702d6181a6547c4262daec6b8171
PReshma-tp <reshma@testpress.in>
2025-08-05 07:45:45 +0000

    add backend email settings

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -137,6 +137,18 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = 'accounts.User'
 
+EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
+
+EMAIL_HOST = config("EMAIL_HOST", default="")
+
+EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
+
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+
+EMAIL_HOST_USER = config("EMAIL_HOST_USER", default="")
+
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD", default="")
+
 MEDIA_URL = "/media/"
 
 MEDIA_ROOT = BASE_DIR / 'media'
