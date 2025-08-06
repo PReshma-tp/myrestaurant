9b6e706314b53e6a658bbe20bbb7480866964a9a
PReshma-tp <reshma@testpress.in>
2025-08-06 09:44:16 +0000

    refactor

--- accounts/views.py
+++ accounts/views.py
@@ -44,6 +44,23 @@ class LogoutView(RedirectView):
 class ProfileView(LoginRequiredMixin, TemplateView):
     template_name = 'accounts/profile.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        user = self.request.user
+
+        sections = {
+            'bookmarks': self._get_bookmarks(user),
+            'visited': self._get_visited(user),
+            'photos': self._get_photos(user),
+            'reviews': self._get_reviews(user),
+        }
+
+        for name, qs in sections.items():
+            context[f"{name}_count"] = qs.count()
+            context[name] = qs[:5]
+
+        return context
+    
     def _get_bookmarks(self, user):
         return Bookmark.objects.filter(user=user) \
             .select_related('restaurant') \
@@ -62,20 +79,3 @@ class ProfileView(LoginRequiredMixin, TemplateView):
         return Review.objects.filter(user=user) \
             .select_related('user') \
             .order_by('-created_at')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        user = self.request.user
-
-        sections = {
-            'bookmarks': self._get_bookmarks(user),
-            'visited': self._get_visited(user),
-            'photos': self._get_photos(user),
-            'reviews': self._get_reviews(user),
-        }
-
-        for name, qs in sections.items():
-            context[f"{name}_count"] = qs.count()
-            context[name] = qs[:5]
-
-        return context
