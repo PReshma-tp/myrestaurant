5015670e722871f8deba87356997c51b537efece
PReshma-tp <reshma@testpress.in>
2025-08-18 05:13:28 +0000

    update static url

--- myrestaurant/settings.py
+++ myrestaurant/settings.py
@@ -128,7 +128,7 @@ LOGOUT_REDIRECT_URL = 'accounts:login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
