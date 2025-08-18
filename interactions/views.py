d5be47d5c8786c3381b025689c0006befc42162a
PReshma-tp <reshma@testpress.in>
2025-08-18 07:06:25 +0000

    add visited restaurants with list view, button UI, and tests

--- interactions/views.py
+++ interactions/views.py
@@ -1,10 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
-from .models import Bookmark
+from .models import Bookmark, Visited
 from django.views import View
 from django.shortcuts import redirect, get_object_or_404
 from restaurants.models import Restaurant
 from django.db.models import Exists, OuterRef, Value, BooleanField
+from django.urls import reverse
 
 # Create your views here.
 
@@ -22,6 +23,23 @@ class BookmarkAnnotationMixin:
         return queryset.annotate(
             is_bookmarked=Value(False, output_field=BooleanField())
         )
+
+class VisitedAnnotationMixin:
+    def annotate_with_visited(self, queryset):
+        if self.request.user.is_authenticated:
+            visited_subquery = Visited.objects.filter(
+                user=self.request.user,
+                restaurant=OuterRef('pk')
+            )
+            queryset = queryset.annotate(
+                is_visited=Exists(visited_subquery)
+            )
+        else:
+            queryset = queryset.annotate(
+                is_visited=Value(False, output_field=BooleanField())
+            )
+
+        return queryset
 
 class BookmarkListView(LoginRequiredMixin,BookmarkAnnotationMixin, ListView):
     model = Restaurant
@@ -42,3 +60,27 @@ class ToggleBookmarkView(LoginRequiredMixin, View):
         if not created:
             bookmark.delete()
         return redirect(request.META.get("HTTP_REFERER", "restaurants:restaurant_list"))
+
+class VisitedListView(LoginRequiredMixin,BookmarkAnnotationMixin, ListView):
+    model = Restaurant
+    template_name = 'visited_restaurants_list.html'
+    context_object_name = 'restaurants'
+
+    def get_queryset(self):
+        queryset= (
+            Restaurant.objects
+            .filter(visited__user=self.request.user)
+            .order_by('-visited__visited_on')
+            .distinct()
+        )
+        return self.annotate_with_bookmarks(queryset)
+
+class ToggleVisitedView(LoginRequiredMixin, View):
+    def post(self, request, pk):
+        restaurant = get_object_or_404(Restaurant, pk=pk)
+        visited_obj, created = Visited.objects.get_or_create(user=request.user, restaurant=restaurant)
+
+        if not created:
+            visited_obj.delete()
+
+        return redirect('restaurants:restaurant_detail', pk=pk)
