04e419a3a11b37c3e19397198dae9b479cc5794f
PReshma-tp <reshma@testpress.in>
2025-08-20 11:02:48 +0000

    fix root page not found

--- myrestaurant/urls.py
+++ myrestaurant/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("restaurants/", include('restaurants.urls', namespace='restaurants')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('interactions/', include('interactions.urls', namespace='interactions')),
+    path('', RedirectView.as_view(url='/restaurants/', permanent=True)),
 ]
 
 if settings.DEBUG:
